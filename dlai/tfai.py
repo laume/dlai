596523ecb13eff6a283f85afdaaff3cbfc7b1363
L U <LU@Ls-MacBook-Pro.local>
2019-08-22 12:45:11 +0300

    changed filter visualization functions

--- dlai/tfai.py
+++ dlai/tfai.py
@@ -1,7 +1,5 @@
 import tensorflow as tf
 import pandas as pd
-from tensorflow import keras
-from keras import backend as K
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -19,7 +17,7 @@ def deprocess_image(x):
     return x
 
 
-def visualise_one_filter(model, layer_name, filter_index, size=150):
+def visualize_one_filter(model, layer_name, filter_index, size=150):
     """
     Builds a loss function that maximizes the activation of the nth filter of the specified layer.
     To plot filter image use plt.imshow(visualise_one_filter(model, layer_name, filter_index, size=150))
@@ -30,12 +28,12 @@ def visualise_one_filter(model, layer_name, filter_index, size=150):
     :return:
     """
     layer_output = model.get_layer(layer_name).output
-    loss = K.mean(layer_output[:, :, :, filter_index])
+    loss = tf.reduce_mean(layer_output[:, :, :, filter_index])
 
-    grads = K.gradients(loss, model.input)[0]
-    grads /= (K.sqrt(K.mean(K.square(grads))) + 1e-5)
+    grads = tf.gradients(loss, model.input)[0]
+    grads /= (tf.sqrt(tf.reduce_mean(tf.square(grads))) + 1e-5)
 
-    iterate = K.function([model.input], [loss, grads])
+    iterate = tf.function([model.input], [loss, grads])
 
     input_img_data = np.random.random((1, size, size, 3)) * 20 + 128
 
@@ -48,11 +46,11 @@ def visualise_one_filter(model, layer_name, filter_index, size=150):
     return deprocess_image(img)
 
 
-def visualise_filters(model, layer_name, size=64, margin=5):
+def visualize_filters(model, layer_name, size=64, margin=5):
     results = np.zeros((8 * size + 7 * margin, 8 * size + 7 * margin, 3))
     for i in range(8):
         for j in range(8):
-            filter_img = visualise_one_filter(model, layer_name, i + (j * 8), size=size)
+            filter_img = visualize_one_filter(model, layer_name, i + (j * 8), size=size)
 
             horizontal_start = i * size + i * margin
             horizontal_end = horizontal_start + size
@@ -63,3 +61,9 @@ def visualise_filters(model, layer_name, size=64, margin=5):
     plt.figure(figsize=(20, 20))
     plt.imshow(results)
 
+
+
+if __name__ == '__main__':
+    layer_name = 'block1_conv1'
+    filter_index = 0
+    # plt.imshow(visualise_one_filter(model, layer_name, filter_index, size=150))
